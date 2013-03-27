37171d903b72794a706f24cd32d3758f3e52be04
myevan <myevan80@gmail.com>
2013-03-27 15:10:12 +0900

    ! korean comments removed

--- UTF8BOM/Connect.cs
+++ UTF8BOM/Connect.cs
@@ -7,20 +7,12 @@ using EnvDTE;
 using EnvDTE80;
 namespace UTF8BOM
 {
-    /// <summary>추가 기능을 구현하는 개체입니다.</summary>
-    /// <seealso class='IDTExtensibility2' />
     public class Connect : IDTExtensibility2
     {
-        /// <summary>추가 기능 개체에 대한 생성자를 구현합니다. 이 메서드 안에 초기화 코드를 배치하십시오.</summary>
         public Connect()
         {
         }
 
-        /// <summary>IDTExtensibility2 인터페이스의 OnConnection 메서드를 구현합니다. 추가 기능이 로드되고 있다는 알림 메시지를 받습니다.</summary>
-        /// <param term='application'>호스트 응용 프로그램의 루트 개체입니다.</param>
-        /// <param term='connectMode'>추가 기능이 로드되는 방법을 설명합니다.</param>
-        /// <param term='addInInst'>이 추가 기능을 나타내는 개체입니다.</param>
-        /// <seealso class='IDTExtensibility2' />
         public void OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
         {
             _applicationObject = (DTE2)application;
@@ -29,10 +21,6 @@ namespace UTF8BOM
             _documentEvents.DocumentSaved += new _dispDocumentEvents_DocumentSavedEventHandler(OnDocumentSaved);
         }
 
-        /// <summary>IDTExtensibility2 인터페이스의 OnDisconnection 메서드를 구현합니다. 추가 기능이 언로드되고 있다는 알림 메시지를 받습니다.</summary>
-        /// <param term='disconnectMode'>추가 기능이 언로드되는 방법을 설명합니다.</param>
-        /// <param term='custom'>호스트 응용 프로그램과 관련된 매개 변수의 배열입니다.</param>
-        /// <seealso class='IDTExtensibility2' />
         public void OnDisconnection(ext_DisconnectMode disconnectMode, ref Array custom)
         {
             _documentEvents.DocumentSaved -= new _dispDocumentEvents_DocumentSavedEventHandler(OnDocumentSaved);
@@ -101,23 +89,14 @@ namespace UTF8BOM
 
         }
 
-        /// <summary>IDTExtensibility2 인터페이스의 OnAddInsUpdate 메서드를 구현합니다. 추가 기능의 컬렉션이 변경되면 알림 메시지를 받습니다.</summary>
-        /// <param term='custom'>호스트 응용 프로그램과 관련된 매개 변수의 배열입니다.</param>
-        /// <seealso class='IDTExtensibility2' />		
         public void OnAddInsUpdate(ref Array custom)
         {
         }
 
-        /// <summary>IDTExtensibility2 인터페이스의 OnStartupComplete 메서드를 구현합니다. 호스트 응용 프로그램에서 로드가 완료되었다는 알림 메시지를 받습니다.</summary>
-        /// <param term='custom'>호스트 응용 프로그램과 관련된 매개 변수의 배열입니다.</param>
-        /// <seealso class='IDTExtensibility2' />
         public void OnStartupComplete(ref Array custom)
         {
         }
 
-        /// <summary>IDTExtensibility2 인터페이스의 OnBeginShutdown 메서드를 구현합니다. 호스트 응용 프로그램이 언로드되고 있다는 알림 메시지를 받습니다.</summary>
-        /// <param term='custom'>호스트 응용 프로그램과 관련된 매개 변수의 배열입니다.</param>
-        /// <seealso class='IDTExtensibility2' />
         public void OnBeginShutdown(ref Array custom)
         {
         }
