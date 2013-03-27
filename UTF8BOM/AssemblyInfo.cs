c228381c1560320685c6d03d82b017dafc01c935
myevan <myevan80@gmail.com>
2013-03-27 15:03:17 +0900

    + UTF8BOM

---
+++ UTF8BOM/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//
+// 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다. 
+// 어셈블리와 관련된 정보를 수정하려면
+// 이 특성 값을 변경하십시오.
+//
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//
+// 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
+//
+//      주 버전
+//      부 버전 
+//      수정 버전
+//      빌드 번호
+//
+// 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 버전이 자동으로 
+// 지정되도록 할 수 있습니다.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+//
+// 어셈블리에 서명하려면 사용할 키를 지정해야 합니다. 
+//  어셈블리 서명에 대한 자세한 내용은 Microsoft .NET Framework 설명서를 참조하십시오.
+//
+// 다음 특성을 사용하면 어떤 키를 사용하여 서명할지 제어할 수 있습니다. 
+//
+// 참고: 
+//   (*) 키를 지정하지 않으면 어셈블리에 서명할 수 없습니다.
+//   (*) KeyName은 사용자 컴퓨터의 CSP(암호화 서비스 공급자)에 설치되어 있는
+//       키를 참조합니다. 
+//   (*) 키 파일과 키 이름 특성이 모두 지정되면 
+//       다음과 같은 프로세스가 생성됩니다.
+//       (1) KeyName이 CSP 안에 있으면 해당 키가 사용됩니다.
+//       (2) KeyName은 없고, KeyFile이 있으면 
+//           파일의 키가 CSP에 설치된 후 사용됩니다.
+//   (*) 서명 연기는 고급 옵션입니다. 자세한 내용은 Microsoft .NET Framework
+//       설명서를 참조하십시오.
+//
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
